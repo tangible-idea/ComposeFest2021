a0a8dd5c1d3c41cb1c87e251ad8993b12e5cef0a
Mark Choi <fantasysa@gmail.com>
2021-11-06 18:57:48 +0900

    1-6.Creating rows and columns.

---
+++ week 1-Jetpack Compose basics/MyCodeWeek1/app/src/main/java/com/example/mycodeweek1/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.mycodeweek1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
+import com.example.mycodeweek1.ui.theme.MyCodeWeek1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyCodeWeek1Theme {
+                // A surface container using the 'background' color from the theme
+                MyApp()
+            }
+        }
+    }
+}
+
+@Composable
+private fun MyApp(names: List<String> = listOf("World", "Compose")) {
+    Column {
+        for (name in names) {
+            Greeting(name = name)
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MyCodeWeek1Theme {
+        Greeting("Android")
+    }
+}
+
+
+data class Message(val author: String, val body: String)
+
+//@PreviewParameter(Message::class)
+@Composable
+fun MessageCard(msg: Message) {
+    Text(text = msg.author)
+    Text(text = msg.body)
+}
+@Composable
+fun Greeting(name: String) {
+    Column(modifier = Modifier.padding(24.dp)) {
+        Text(text = "Hello,")
+        Text(text = name)
+    }
+}
