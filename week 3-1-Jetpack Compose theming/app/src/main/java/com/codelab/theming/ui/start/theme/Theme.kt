df21fa89ee6f4cff1a157f1facee580ee8867fc4
Mark Choi <fantasysa@gmail.com>
2021-12-04 17:35:41 +0900

    week 3-1 done.

--- week 3-1-Jetpack Compose theming/app/src/main/java/com/codelab/theming/ui/start/theme/Theme.kt
+++ week 3-1-Jetpack Compose theming/app/src/main/java/com/codelab/theming/ui/start/theme/Theme.kt
@@ -1,20 +1,4 @@
-/*
- * Copyright 2020 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package com.codelab.theming.ui.finish.theme
+package com.codelab.theming.ui.start.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
@@ -31,7 +15,7 @@ private val LightColors = lightColors(
     secondaryVariant = Red900,
     onSecondary = Color.White,
     error = Red800
-)
+                                     )
 
 private val DarkColors = darkColors(
     primary = Red300,
@@ -40,17 +24,18 @@ private val DarkColors = darkColors(
     secondary = Red300,
     onSecondary = Color.Black,
     error = Red200
-)
+                                   )
 
 @Composable
 fun JetnewsTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable () -> Unit
-) {
+    content: @Composable () -> Unit) {
     MaterialTheme(
         colors = if (darkTheme) DarkColors else LightColors,
         typography = JetnewsTypography,
         shapes = JetnewsShapes,
         content = content
-    )
+                 )
 }
+
+
